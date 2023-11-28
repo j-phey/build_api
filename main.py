42e15edf987d3c31fd7223cda0f511f503f75c17
jjjjjjpppppp <jonphey91@gmail.com>
2023-11-28 18:00:56 +1100

    Added controllers

--- main.py
+++ main.py
@@ -14,5 +14,11 @@ def create_app():
     # creating a generic db object that we can import into our models code
     # creating our database object! This allows us to use our ORM
     db.init_app(app)
-   
+
+    # import the controllers and activate the blueprints
+    from controllers import registerable_controllers
+
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+
     return app
