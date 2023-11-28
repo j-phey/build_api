da3cc4a8e7acd16461aca953cce17f0bf3c515a5
jjjjjjpppppp <jonphey91@gmail.com>
2023-11-28 16:42:02 +1100

    Added a models folder

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def create_app():
+    
+    # Creating the flask app object - this is the core of our app!
+    app = Flask(__name__)
+
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating a generic db object that we can import into our models code
+    # creating our database object! This allows us to use our ORM
+    db.init_app(app)
+   
+    return app
