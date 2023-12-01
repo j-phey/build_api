ca6716344ae185669dbed340738de7220c588cff
jjjjjjpppppp <jonphey91@gmail.com>
2023-12-01 15:48:30 +1100

    Allowed nesting of user data during GET cards

--- schemas/card_schema.py
+++ schemas/card_schema.py
@@ -1,10 +1,12 @@
 from main import ma
+from marshmallow import fields # needed for nested fields in request
 
 #create the Card Schema with Marshmallow, it will provide the serialization needed for converting the data into JSON
 class CardSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ("id", "title", "description", "date", "status", "priority")
+        fields = ("id", "title", "description", "date", "status", "priority", "user")
+    user =  fields.Nested("UserSchema", only=("email",)) # Don't display everything - only the email
 
 #single card schema, when one card needs to be retrieved
 card_schema = CardSchema()
